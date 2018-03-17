cf120218a353627405772a8270975ee2715195af
Jihye Eom <>
2018-03-18 03:27:13 +0900

    initial project checkin

---
+++ Xamarin_LayoutXAML/Xamarin_LayoutXAML/Xamarin_LayoutXAMLPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin_LayoutXAML
+{
+    public partial class Xamarin_LayoutXAMLPage : ContentPage
+    {
+        public Xamarin_LayoutXAMLPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
