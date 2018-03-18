18ab5a1a625f9d719300d1b800c481ffac9f0848
Jihye Eom <>
2018-03-18 21:49:37 +0900

    padding

--- Xamarin_LayoutXAML/Xamarin_LayoutXAML/Xamarin_LayoutXAMLPage.xaml.cs
+++ Xamarin_LayoutXAML/Xamarin_LayoutXAML/Xamarin_LayoutXAMLPage.xaml.cs
@@ -1,12 +1,24 @@
 ﻿using Xamarin.Forms;
+//using Xamarin.Forms.Xaml;
+
 
 namespace Xamarin_LayoutXAML
 {
+    //[XamlCompilation(XamlCompilationOptions.Skip)]
     public partial class Xamarin_LayoutXAMLPage : ContentPage
     {
         public Xamarin_LayoutXAMLPage()
         {
             InitializeComponent();
+
+            /*
+            var x = new OnPlatform<Thickness>
+            {
+                iOS = new Thickness(0, 20, 0, 0),
+                Android = new Thickness(0, 40, 0, 0)
+            };
+            Padding = x;
+            */
         }
     }
 }
